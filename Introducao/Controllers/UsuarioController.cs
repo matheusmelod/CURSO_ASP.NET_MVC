9c9fe77813397e022a72306336e719b8dca314c9
Matheus <meloomatheus@gmail.com>
2017-12-15 16:54:09 -0200

    Commit Aula 32.
    Inserindo valiidações no programa

---
+++ Introducao/Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using Introducao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Usuario()
+        {
+            var usuario = new Usuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Usuario(Usuario usuario)
+        {
+            if(string.IsNullOrEmpty(usuario.Nome))
+            {
+                ModelState.AddModelError("Nome", "O campo Nome é obrigatorio");
+            }
+            if(ModelState.IsValid)
+            {
+                return View("Resultado", usuario);
+            }
+            return View(usuario);
+        }
+    
+        public ActionResult Resultado(Usuario usuario)
+        {
+
+            return View(usuario);
+        }
+    }
+}
